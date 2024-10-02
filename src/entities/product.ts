ed213d507fe901ca6a216f29cc7bf2ea0012d0bd
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-01 23:22:46 -0300

    Refactorizar classes Cart e Product para melhorar a funcionalidade do carrinho de compras

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import { Cart } from "./cart";
 
 export class Product {
   private _id: string = uuidv4();
@@ -7,6 +8,7 @@ export class Product {
   private _category: string;
   private _imageUrl: string;
   private _quantity: number = 0;
+  private _total: number = 0;
 
   constructor(name: string, price: number, category: string, imageUrl: string) {
     this._name = name;
@@ -15,12 +17,66 @@ export class Product {
     this._imageUrl = imageUrl;
   }
 
+  toHTML() {
+    const productListHTML = document.getElementById("product-list");
+
+    if (!productListHTML) return;
+
+    const productHTML = document.createElement("li");
+    productHTML.id = this._id;
+
+    productHTML.innerHTML = `
+        <div class="flex flex-col h-[260px] w-[250px] border border-black">
+          <div class="mb-6 relative h-full border border-red-600">
+            <div id="button-add-to-cart" class="button center text-xs">Add to Cart</div>
+          </div>
+
+          <div class="flex flex-col">
+            <span class="product-category">${this._category}</span>
+            <span class="product-name">${this._name}</span>
+            <span class="product-price">${this.price}</span>
+          </div>
+        </div>
+      `;
+
+    productHTML
+      .querySelector("#button-add-to-cart")
+      ?.addEventListener("click", () => this.incrementQuantity());
+
+    productListHTML.appendChild(productHTML);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  updateTotal() {
+    this._total = this._price * this._quantity;
+  }
+
+  incrementQuantity() {
+    console.log("entrou aqui");
+    this._quantity += 1;
+    this.updateTotal();
+
+    Cart.addToCart(this);
+  }
+
+  decrementQuantity() {
+    this._quantity -= 1;
+    this.updateTotal();
+  }
+
+  get total() {
+    return this._total;
+  }
+
   get quantity() {
     return this._quantity;
   }
 
   set quantity(quantity: number) {
-    this._quantity += quantity;
+    this._quantity = quantity;
   }
 
   get name() {
---
+++ src/tempCodeRunnerFile.ts
@@ -0,0 +1,3 @@
+a.incrementQuantity();
+banana.incrementQuantity();
+banana.incrementQuantity();
