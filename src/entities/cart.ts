b1eeaf869449a6ee40d68d500a5f5fddc157efaf
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-01 23:22:46 -0300

    Refactorizar classes Cart e Product para melhorar a funcionalidade do carrinho de compras

--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -1,21 +1,45 @@
 import { Product } from "./product";
 
 export class Cart {
-  private _products: Product[] = [];
-  private _total: number = 0;
+  private static _products: Product[] = [];
+  private static _orderTotal: number = 0;
+  private static _quantityTotal: number = 0;
 
-  addToCart(product: Product) {
-    // Atualiza o valor total da compra
-    this._total += product.price;
-    // Adiciona o produto ao carrinho
-    this._products.push(product);
+  static calculateTotal() {
+    this._orderTotal = 0;
+    this._quantityTotal = 0;
+
+    this._products.forEach((product) => {
+      this._orderTotal += product.total;
+      this._quantityTotal += product.quantity;
+    });
   }
 
-  get products() {
+  static removeProduct(product: Product) {
+    // Remove um produto do carrinho
+    this._products = this._products.filter((item) => item.id !== product.id);
+    this.calculateTotal();
+  }
+
+  static addToCart(product: Product) {
+    // Verificar se o produto existe no carrinho
+    const productExists = this._products.includes(product);
+
+    // Se o produto não estiver no carrinho, execute
+    if (!productExists) {
+      this._products.push(product);
+    }
+
+    // Atualiza o valor total da compra
+    this.calculateTotal();
+    console.log(JSON.parse(JSON.stringify(Cart._products)));
+  }
+
+  static get products() {
     return this._products;
   }
 
-  get total() {
-    return this._total;
+  static get total() {
+    return this._orderTotal;
   }
 }
