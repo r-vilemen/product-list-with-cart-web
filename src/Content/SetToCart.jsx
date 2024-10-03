46099ae972246146ad82e0f6dfefd948f0bf157f
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-03 05:59:42 -0300

    Refactorizar componente SetToCart e atualizar ícones de carrinho

--- src/Content/SetToCart.jsx
+++ src/Content/SetToCart.jsx
@@ -1,64 +1,51 @@
-import React, { useState, useEffect } from "react";
-import cart from "../assets/images/icon-add-to-cart.svg";
-import "./Dessert.css";
+import React, { useState } from "react";
 
-function SetToCart({ name, price, addToCart, removeFromCart, cartItems, image }) {
-    const [addedToCart, setAddedToCart] = useState(false);
-    const [quantity, setQuantity] = useState(1);
+const SetToCart = ({ name, price, image, cart, addToCart, removeFromCart }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [addedToCart, setAddedToCart] = useState(false);
 
-    useEffect(() => {
-        const itemInCart = cartItems.find(item => item.name === name);
-        if (itemInCart) {
-            setAddedToCart(true);
-            setQuantity(itemInCart.quantity);
-        } else {
-            setAddedToCart(false);
-            setQuantity(1);
-        }
-    }, [cartItems, name]);
+  const handleClick = () => {
+    setAddedToCart(true);
+    addToCart({ name, price, quantity, image });
+  };
 
-    const handleClick = () => {
-        if (!addedToCart) {
-            setAddedToCart(true);
-            addToCart({ name, price, quantity, image });
-        }
-    };
+  const handleIncrease = () => {
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+    addToCart({ name, price, quantity: newQuantity, image });
+  };
 
-    const handleIncrease = () => {
-        const newQuantity = quantity + 1;
-        setQuantity(newQuantity);
-        addToCart({ name, price, quantity: newQuantity, image });
-    };
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      const newQuantity = quantity - 1;
+      setQuantity(newQuantity);
+      addToCart({ name, price, quantity: newQuantity, image });
+    } else {
+      setAddedToCart(false);
+      setQuantity(1);
+      removeFromCart(name);
+    }
+  };
 
-    const handleDecrease = () => {
-        if (quantity > 1) {
-            const newQuantity = quantity - 1;
-            setQuantity(newQuantity);
-            addToCart({ name, price, quantity: newQuantity, image });
-        }
-    };
-
-    return (
-        <div className="dessert--CardButton">
-            {addedToCart ? (
-                <button className="cart--Button--quantity">
-                    <div onClick={handleDecrease} className="cart--Button--quantityMinus">
-                        <i className="fa-solid fa-minus"></i>
-                    </div>
-                    <div>
-                        {quantity}
-                    </div>
-                    <div onClick={handleIncrease} className="cart--Button--quantityPlus">
-                        <i className="fa-solid fa-plus"></i>
-                    </div>
-                </button>
-            ) : (
-                <button onClick={handleClick} className="cart--Button--add">
-                    <img src={cart} alt="cart" /> Add to Cart
-                </button>
-            )}
-        </div>
-    );
-}
+  return (
+    <div className="dessert--CardButton">
+      {addedToCart ? (
+        <button className="cart--Button--quantity">
+          <div onClick={handleDecrease} className="cart--Button--quantityMinus">
+            <i className="fas fa-minus"></i>
+          </div>
+          <div>{quantity}</div>
+          <div onClick={handleIncrease} className="cart--Button--quantityPlus">
+            <i className="fas fa-plus"></i>
+          </div>
+        </button>
+      ) : (
+        <button onClick={handleClick} className="cart--Button--add">
+          <i className="fas fa-shopping-cart"></i> Add to Cart
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default SetToCart;
