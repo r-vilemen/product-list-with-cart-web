966febeaf07350547b88fadf8d8c0b38349a66d1
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-03 05:40:50 -0300

    Add necessary files and configurations

---
+++ src/mainContent/Dessert.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../mainContent/Dessert.css';
+import SetToCart from './SetToCart';
+
+const Dessert = ({ name, category, price, image, addToCart, cartItems, removeFromCart }) => {
+    return (
+        <div className='dessert--Card'>
+            <img src={image.desktop} alt={name} />
+            <SetToCart 
+                name={name} 
+                price={price} 
+                addToCart={addToCart} 
+                cartItems={cartItems} 
+                removeFromCart={removeFromCart}
+                image={image} // Passe a imagem para SetToCart
+            />
+            <p>{category}</p>
+            <h2>{name}</h2>
+            <h4>${price.toFixed(2)}</h4>
+        </div>
+    );
+};
+
+export default Dessert;
